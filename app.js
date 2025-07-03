cf35bac5401242b341fd6a9c1690414006875aa1
adelm <adel@xolog.com>
2025-07-03 21:29:53 +0300

    revert app header changes

--- app.js
+++ app.js
@@ -40,13 +40,6 @@ app.use("/api/v1/tasks", taskRouter);
 app.use("/api/v1/admin/reports", reportRouter);
 app.use("/api/v1/reports", reportRouter);
 
-// Serve static files from the "public" directory
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://myaccount.xolog.com');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
-});
 
 //Server uploads folder
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
