16643fca28a4f64ebce234f34ca4515a98942082
adelm <adel@xolog.com>
2025-09-19 20:00:59 +0300

    fix(tobeLoaded): restore user ID validation logic in tobeLoadedReport function

--- controllers/Clients/tobeLoadedReportController.js
+++ controllers/Clients/tobeLoadedReportController.js
@@ -16,12 +16,12 @@ export const tobeLoadedReport = async (req, res) => {
 
     console.log("userId", userId);
 
-    // if (userId == null || userId === 0 || userId === "0") {
-    //   return res.status(400).json({
-    //     success: false,
-    //     message: "User ID is required and must be valid",
-    //   });
-    // }
+    if (userId == null || userId === 0 || userId === "0") {
+      return res.status(400).json({
+        success: false,
+        message: "User ID is required and must be valid",
+      });
+    }
 
     if (userId) {
       filter.CustomerId = userId;
